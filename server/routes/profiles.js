44a213cc75c2f07ba7ca0563a6899740787af836
tamiryul <99034345+tamiryul@users.noreply.github.com>
2023-05-08 22:12:28 +0300

    Update profiles.js

--- server/routes/profiles.js
+++ server/routes/profiles.js
@@ -1,36 +1,24 @@
-///// ראוטר שינהל את טבלת profiles
 module.exports = app => {
-    const profiles = require('./../controllers/profiles.js') //// נצרוך את הcontrollers
+    const profiles = require('./../controllers/profiles.js')
     var router = require("express").Router();
 
-///// כשיש לי ראוט כזה איזה פונקציה צריך להפעיל
-
-///// יצירה חדשה
     router.post("/", profiles.create);
 
-///// אחזור כל המידע
     router.get("/", profiles.findAll);
 
-///// אחזור מידע בודד
     router.get("/:id", profiles.findOne);
 
-///// עדכון
     router.put("/:id", profiles.update);
 
-///// מחיקה
     router.delete("/:id", profiles.delete);
 
-///// מחיקת הכל
     router.delete("/", profiles.deleteAll);
 
-///// תציג כל מה שפורסם
     router.get("/published", profiles.findAllPublished);
 
-///// כל מערכת ההפניות יכילו את זה ואז את הראוט
     app.use('/api/profiles', router);
 
     
-/// נבנה הפניה פשוטה
     app.get("/", (req, res)=>{
         res.json({message:"welcome to my web"});
     })
