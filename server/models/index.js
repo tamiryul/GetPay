dd5b0ba6f02c62120431352e20652c9bef5eb874
tamiryul <99034345+tamiryul@users.noreply.github.com>
2023-05-08 22:10:32 +0300

    Update index.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,10 +1,8 @@
 ///// define sequelize- a node.js module that allows me to work easier with database.
-///// sequelize- אובייקט שמחזיק את SEQUELIZE
 
 const dbConfig = require('./../config/db.config.js');
 const Sequelize = require("sequelize");
 
-///// ספריית צד שלישי שעוזרת לנו לעבוד עם מסד הנתונים, שואבת מידע מקובץ הקונפיגורציה
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD,  {
     host: dbConfig.HOST,
     port: 3306,
@@ -18,12 +16,11 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD,  
     }
 })
 
-///// אובייקט שמכיל את המפתחות הבאים: sequelize + Sequelize + profiles + products+ debts
 const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
-db.profiles = require('./profiles.js')(sequelize, Sequelize);////יוצר קשר עם דף profiles
-db.products = require('./products.js')(sequelize, Sequelize);////יוצר קשר עם דף products
-db.debts = require('./debts.js')(sequelize, Sequelize);////יוצר קשר עם דף debts
+db.profiles = require('./profiles.js')(sequelize, Sequelize);
+db.products = require('./products.js')(sequelize, Sequelize);
+db.debts = require('./debts.js')(sequelize, Sequelize);
 
 module.exports = db;
