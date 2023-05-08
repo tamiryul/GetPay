e8e57dfddf9517aeb8a12368e892ffae6d2ce7ed
tamiryul <99034345+tamiryul@users.noreply.github.com>
2023-05-08 22:06:53 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,30 +6,22 @@ const app = express();
 
 let corsOption = {origin: '*'}
 
-/////לאילו מקורות נאפשר גישה לשרת
 app.use(cors(corsOption))
 
-/////נאפשר לטפל בבקשות בצורת- JSON
 app.use(express.json());
 
-/////נאפשר לטפל בבקשות בצורת - URL ENCODED
 app.use(express.urlencoded({extended: true}));
 
 
-///// נכלול את מערכת הראוט
 require("./routes/profiles.js")(app);
 
-///// נכלול את מערכת הראוט
 require("./routes/products.js")(app);
 
-///// נכלול את מערכת הראוט
 require("./routes/debts.js")(app);
 
-///// סנכרון עם מסד הנתונים 
 db.sequelize.sync();
 
 
-// // ///// נבנה הפניה פשוטה
 // app.get("/", (req, res)=>{
 //     res.json({message:"welcome to my web"});
 // })
